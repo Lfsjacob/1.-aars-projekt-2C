368d72197c9b11542e75a5af7a5fc25972a24107
IoTkea2324 <mabe0002@stud.kea.dk>
2024-05-27 19:22:46 +0200

    updateringtidspunktuaering

--- login.py
+++ login.py
@@ -2,17 +2,11 @@ from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user
 
-# Opretter en flask applikation
 app = Flask(__name__)
-
-# fortæller hvilken database flask-sqlalchemy skal forbinde
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
-# Skriv en secret key
 app.config["SECRET_KEY"] = "abc"
-# Opretter en instans af SQLAlchemy
 db = SQLAlchemy()
 
-# LoginManager skal bruges til app'en for at kunne logge ind og ud
 login_manager = LoginManager()
 login_manager.init_app(app)
 
@@ -34,24 +28,19 @@ def loader_user(user_id):
     return Users.query.get(int(user_id))
 
 @app.route('/register', methods=["GET", "POST"])
-def registrer():
-    # hvis brugeren har lavet POST request, lav en ny bruger
-    if request.method == "POST":
-        user = Users(username=request.form.get("username"),
-                     password=request.form.get("password"))
-        # tilføj bruger til database
-        db.session.add(user)
-        # commit'er ændringer
-        db.session.commit()
+def register():
+	if request.method == "POST":
+		user = Users(username=request.form.get("username"),
+					password=request.form.get("password"))
+		db.session.add(user)
+		db.session.commit()
         # når bruger account er oprettet bliver man redirected til login route (som bliver lavet senere)
-        return redirect(url_for("login"))
-    # indlæser sign_up template hvis brugeren laver et GET request
-    return render_template("sign_up.html")
+		return redirect(url_for("login"))
+	return render_template("sign_up.html")
 
 # Opretter en login route 
 @app.route('/login', methods=["GET", "POST"])
 def login():
-    # hvis der er lavet et POST request, find brugeren ved at filtrere efter brugernavn
     if request.method == "POST":
         user = Users.query.filter_by(
             username=request.form.get("username")).first()
