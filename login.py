eca42c900356140b83f0c0fef8c4219d5c92398a
IoTkea2324 <mabe0002@stud.kea.dk>
2024-05-27 19:42:28 +0200

    update

--- login.py
+++ login.py
@@ -33,17 +33,22 @@ login_manager.login_view = 'login'
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
-	if request.method == "POST":
-		username = request.form.get("username")
-		password = request.form.get("password")
-		try:
-            conn = sqlite3.connect(database="adminbrugere.db")
-            cur = conn.cursor()
-            query = f"INSERT INTO users(username, password) VALUES(?, ?)"
-            data = (username, password)
-            cur.execute(query, data)
-            conn.commit()
-		except:
+	# if request.method == "POST":
+	username = request.form.get('brugernavn')
+	password = request.form.get('password')
+	print(f"{username = }")
+	print(f"{password = }  ")
+	try:
+		conn = sqlite3.connect(database="adminbrugere.db")
+		cur = conn.cursor()
+		query = f"INSERT INTO users(username, password) VALUES(?, ?)"
+		data = (username, password)
+		cur.execute(query, data)
+		conn.commit()
+	except sqlite3.Error as e:
+		conn.rollback()
+		print(f"Kunne ikke oprette {e}")
+
 	
 	
     
@@ -53,7 +58,7 @@ def register():
 		# db.session.add(user)
 		# db.session.commit()
 		# return redirect(url_for("home.html"))
-	return render_template("login.html")
+	return render_template("register.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
