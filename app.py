9ea1fdde42160827e2970e3fe0dcb7f9c8b32e73
IoTkea2324 <mabe0002@stud.kea.dk>
2024-05-27 19:23:13 +0200

    opdatering

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+       return render_template('login.html')
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+        return render_template('register.html')
+    
 app.run(debug=True)
