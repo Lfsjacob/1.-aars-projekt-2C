1a3e09e55beefb2a8f35edc097cb1f3b0609a2f1
IoTkea2324 <mabe0002@stud.kea.dk>
2024-05-27 20:28:03 +0200

    Done for mandag (Specielt tak til Jacob)

--- login.py
+++ login.py
@@ -1,86 +1,62 @@
 import sqlite3
 from flask import Flask, render_template, request, url_for, redirect
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, logout_user
+
+master_key = "magickey"
 
 app = Flask(__name__)
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///adminbrugere.db"
 app.config["SECRET_KEY"] = "abc"
-# db = SQLAlchemy()
-
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
-
-
-# class Users(UserMixin, db.Model):
-# 	id = db.Column(db.Integer, primary_key=True)
-# 	username = db.Column(db.String(250), unique=True, nullable=False)
-# 	password = db.Column(db.String(250), nullable=False)
-
-
-# db.init_app(app)
-
-
-# with app.app_context():
-	# db.create_all()
-
-
-# @login_manager.user_loader
-# def loader_user(user_id):
-# 	return Users.query.get(int(user_id))
 
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
-	# if request.method == "POST":
-	username = request.form.get('brugernavn')
-	password = request.form.get('password')
-	print(f"{username = }")
-	print(f"{password = }  ")
-	try:
-		conn = sqlite3.connect(database="adminbrugere.db")
-		cur = conn.cursor()
-		query = f"INSERT INTO users(username, password) VALUES(?, ?)"
-		data = (username, password)
-		cur.execute(query, data)
-		conn.commit()
-	except sqlite3.Error as e:
-		conn.rollback()
-		print(f"Kunne ikke oprette {e}")
+	global master_key
+	received_key = request.form.get('master_password')
+	if request.method == "POST" and received_key == master_key:
+		username = request.form.get('brugernavn')
+		password = request.form.get('password')
+		print(f"{username = }")
+		print(f"{password = }  ")
+		try:
+			conn = sqlite3.connect(database="instance/adminbrugere.db")
+			cur = conn.cursor()
+			query = f"INSERT INTO users(username, password) VALUES(?, ?)"
+			data = (username, password)
+			cur.execute(query, data)
+			conn.commit()
+		except sqlite3.Error as e:
+			conn.rollback()
+			print(f"Kunne ikke oprette {e}")
 
 	
-	
-    
 
-		# user = Users(username=request.form.get("username"),
-		# 			password=request.form.get("password"))
-		# db.session.add(user)
-		# db.session.commit()
-		# return redirect(url_for("home.html"))
 	return render_template("register.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
 	if request.method == "POST":
-		# user = Users.query.filter_by(username=request.form.get("username")).first()
-		# if user and user.password == request.form.get("password"):
-		# 	login_user(user)
-			return redirect(url_for("login.html"))
+		username = request.form.get('brugernavn')
+		password = request.form.get('password')
+		conn= sqlite3.connect(database="instance/adminbrugere.db")
+		cur = conn.cursor()
+		query = f"SELECT * FROM users"
+		data = (username, password)
+		cur.execute(query, data)
+		for row in cur:
+			if row[0] == username and row[1] == password:
+				return redirect("/home", code=302)
+
 	return render_template("login.html")
 
-
-@app.route("/logout")
-def logout():
-	logout_user()
-	return redirect(url_for("login.html"))
 
 
 @app.route("/")
-def home():
+def hjem():
 	return render_template("login.html")
 
+@app.route("/home")
+def home():
+	return render_template("home.html")
 
 if __name__ == "__main__":
 	app.run()
