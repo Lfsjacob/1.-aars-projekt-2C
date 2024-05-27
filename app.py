9f20bd441f510889f1f9752442e063975de623ed
Lfsjacob <jacobtoender@live.dk>
2024-05-27 19:25:48 +0200

    Transfer mellem tables virker

--- app.py
+++ app.py
@@ -4,41 +4,42 @@ import flask
 
 app = flask.Flask(__name__)
 
-def match(produktnavn, produktnummer, antal, mål, producent,produktkategori, pris):
-    produktnavn = produktnavn
-    antal = antal
-    mål = mål
+def match(produktinfo, tablename):
+    print("Tjekker rute")
     new = 0
+    route = tablename
     try:
         conn = sqlite3.connect(database="GTV_Tagdækning_ApS.db")
         cur = conn.cursor()
-        cur.execute('SELECT * FROM Bestillingsoversigt')
+        cur.execute(f'SELECT * FROM {tablename}')
         # for row in cur:
         results = cur.fetchall()
         for result in results:
             print(f"{result[0]} {result[1] = }  {result[4] = }")
-            if result[1] == produktnavn and result[4] == mål:
-                print(f"{antal = }")
-                add_to_product(result[0], int(result[3] + int(antal)))
+            if result[1] == produktinfo[0] and result[4] == produktinfo[3]:
+                print(f"{produktinfo[2] = }")
+                add_to_product(result[0], int(result[3] + int(produktinfo[2])), tablename)
                 print("JA!")
                 conn.commit()
                 new = 1
                 break
         if new == 0:
-            add_new_product(produktnavn, produktnummer, antal, mål, producent,produktkategori, pris)
+            add_new_product(produktinfo, tablename)
         print(results)
         conn.commit()
     except Exception as e:
         print(f"Der skete en fejl: {e}")
 
-
-def add_to_product(id, antal):
+def add_to_product(id, antal, tablename):
+    print("Forsøger at tilføje til produkt")
     try:
+        print("Åbner databasen")
         conn = sqlite3.connect(database="GTV_Tagdækning_ApS.db")
-        query = f"""UPDATE Bestillingsoversigt SET Antal = ? WHERE ID = ?"""
+        query = f"""UPDATE {tablename} SET Antal = ? WHERE ID = ?"""
         data = antal, id
 
         try:
+            print("Forsøger at indsætte")
             cur = conn.cursor()
             cur.execute(query, data)
             conn.commit()
@@ -52,13 +53,15 @@ def add_to_product(id, antal):
         conn.rollback()
         print(f"Kunne ikke indsætte!: {e}")
 
-def add_new_product(produktnavn, produktnummer, antal, mål, producent, produktkategori, pris):
+def add_new_product(produktinfo, tablename):
+    print("Forsøger at indsætte nyt produkt")
     try:
+        print("Åbner databasen")
         conn = sqlite3.connect(database="GTV_Tagdækning_ApS.db")
-        query = """INSERT INTO Bestillingsoversigt(Produktnavn, Produktnummer, Antal, Mål, Producent, Produktkategori, Pris) VALUES(?, ?, ?, ?, ?, ?, ?)"""
-        data = produktnavn, produktnummer, antal, mål, producent, produktkategori, pris
-
+        query = f"""INSERT INTO {tablename}(Produktnavn, Produktnummer, Antal, Mål, Producent, Produktkategori, Pris) VALUES(?, ?, ?, ?, ?, ?, ?)"""
+        data = tuple(produktinfo)
         try:
+            print("Forsøger at indsætte")
             cur = conn.cursor()
             cur.execute(query, data)
             conn.commit()
@@ -72,9 +75,28 @@ def add_new_product(produktnavn, produktnummer, antal, mål, producent, produktk
         conn.rollback()
         print(f"Kunne ikke indsætte!: {e}")
 
-def bestilling_til_lager():
-    conn = sqlite3.connect(database="GTV_Tagdækning_ApS.db")
-    query = """"""
+def delete_amount(id, tablename):
+    print(f"Forsøger at sætte {id} i {tablename} til 0")
+    try:
+        print("Åbner databasen")
+        conn = sqlite3.connect(database="GTV_Tagdækning_ApS.db")
+        query = f"""UPDATE {tablename} SET Antal = ? WHERE ID = ?"""
+        data = 0, id
+
+        try:
+            print("Forsøger at indsætte")
+            cur = conn.cursor()
+            cur.execute(query, data)
+            conn.commit()
+        except sqlite3.Error as e:
+            conn.rollback()
+            print(f"add_to_product:  Kunne ikke indsætte!: {e}")
+        except Exception as e:
+            print(f"Der skete en fejl: {e}")
+
+    except sqlite3.Error as e:
+        conn.rollback()
+        print(f"Kunne ikke indsætte!: {e}")
 
 @app.route('/', methods=['POST', 'GET'])
 def home():
@@ -114,9 +136,31 @@ def bestilte_varer_post():
     produktinfo = [produktnavn, produktnummer, antal, mål, producent,produktkategori, pris]
     print(produktinfo)
     
-    match(produktnavn, produktnummer, antal, mål, producent,produktkategori, pris)
+
+    
+    if produktnavn is None:
+        id = int(flask.request.form.get('fjern_vare'))
+        try:
+            conn = sqlite3.connect(database="GTV_Tagdækning_ApS.db")
+            query = f"""SELECT Produktnavn, Produktnummer, Antal, Mål, Producent, Produktkategori, Pris FROM Bestillingsoversigt WHERE ID = ?"""
+            data = id
+            cur = conn.cursor()
+            cur.execute(query, (data,))
+            conn.commit
+            results = cur.fetchall()
+            result = results[0]
+            print(f"{result = }")
+            match(list(result), "Lageroversigt")
+            delete_amount(id, "Bestillingsoversigt")
+        except Exception as e:
+                print(f"bvtl   Der skete en fejl: {e}")
+        
+
+    else:
+        match(produktinfo, "Bestillingsoversigt")
 
     return bestilte_varer()
+    
 
 @app.route('/prisliste', methods=['POST', 'GET'])
 def prisliste():
