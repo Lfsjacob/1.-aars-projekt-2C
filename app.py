63a43a65698caa3d1daf47669ef61d18e95bcf73
Sebastian <sebastiangraven@proton.me>
2024-05-28 11:32:24 +0200

    Ændringer

--- app.py
+++ app.py
@@ -5,7 +5,75 @@ import sqlite3
 app = Flask(__name__)
 
 # Functions
+def match(produktnavn, produktnummer, antal, mål, producent,produktkategori, pris):
+    produktnavn = produktnavn
+    antal = antal
+    mål = mål
+    new = 0
+    try:
+        conn = sqlite3.connect(database="database/ny_database.db")
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM Lageroversigt')
+        # for row in cur:
+        results = cur.fetchall()
+        for result in results:
+            print(f"{result[0]} {result[1] = }  {result[4] = }")
+            if result[1] == produktnavn and result[4] == mål:
+                print(f"{antal = }")
+                add_to_product(result[0], int(result[3] + int(antal)))
+                print("JA!")
+                conn.commit()
+                new = 1
+                break
+        if new == 0:
+            add_new_product(produktnavn, produktnummer, antal, mål, producent,produktkategori, pris)
+        print(results)
+        conn.commit()
+    except Exception as e:
+        print(f"Der skete en fejl: {e}")
 
+def add_to_product(id, antal):
+    try:
+        conn = sqlite3.connect(database="database/ny_database.db")
+        query = f"""UPDATE Lageroversigt SET Antal = ? WHERE ID = ?"""
+        data = antal, id
+
+        try:
+            cur = conn.cursor()
+            cur.execute(query, data)
+            conn.commit()
+        except sqlite3.Error as e:
+            conn.rollback()
+            print(f"add_to_product:  Kunne ikke indsætte!: {e}")
+        except Exception as e:
+            print(f"Der skete en fejl: {e}")
+
+    except sqlite3.Error as e:
+        conn.rollback()
+        print(f"Kunne ikke indsætte!: {e}")
+
+def add_new_product(produktnavn, produktnummer, antal, mål, producent, produktkategori, pris):
+    try:
+        conn = sqlite3.connect(database="database/ny_database.db")
+        query = """INSERT INTO Lageroversigt(Produktnavn, Produktnummer, Antal, Mål, Producent, Produktkategori, Pris) VALUES(?, ?, ?, ?, ?, ?, ?)"""
+        query2 = "SELECT ID, Produktnavn, Produktnummer, Antal, Mål, Producent, Produktkategori, Pris FROM Lageroversigt ORDER BY id ASC"
+        data = produktnavn, produktnummer, antal, mål, producent, produktkategori, pris
+
+        try:
+            cur = conn.cursor()
+            cur.execute(query, data)
+            conn.commit()
+            # cur.execute(query2)
+            # data_rows = cur.fetchall()
+        except sqlite3.Error as e:
+            conn.rollback()
+            print(f"Overordnet:  Kunne ikke indsætte!: {e}")
+        except Exception as e:
+            print(f"Der skete en fejl: {e}")
+
+    except sqlite3.Error as e:
+        conn.rollback()
+        print(f"Kunne ikke indsætte!: {e}")
 
 # Routes
 @app.route('/')
@@ -16,7 +84,7 @@ def index():
 def lager():
     data_rows = []
     conn = sqlite3.connect(database='database/ny_database.db')
-    QUERY = "SELECT Produktnavn, Produktnummer, Antal, Mål, Producent, Produktkategori, Pris FROM Lageroversigt ORDER BY id DESC"
+    QUERY = "SELECT ID, Produktnavn, Produktnummer, Antal, Mål, Producent, Produktkategori, Pris FROM Lageroversigt ORDER BY id ASC"
 
     try:
         cur = conn.cursor()
@@ -50,7 +118,7 @@ def lager():
         conn = sqlite3.connect(database='database/ny_database.db')
         query = "INSERT INTO Lageroversigt(Produktnavn, Produktnummer, Antal, Mål, Producent, Produktkategori, Pris) VALUES (?, ?, ?, ?, ?, ?, ?)"
         data = (produktnavn, produktnummer, antal, mål, producent, produktkategori, pris)
-        query2 = "SELECT Produktnavn, Produktnummer, Antal, Mål, Producent, Produktkategori, Pris FROM Lageroversigt ORDER BY id DESC"
+        query2 = "SELECT ID, Produktnavn, Produktnummer, Antal, Mål, Producent, Produktkategori, Pris FROM Lageroversigt ORDER BY id ASC"
 
         try:
             cur = conn.cursor()
@@ -70,7 +138,51 @@ def lager():
             cur.close()
             conn.close()
 
+        fjern_vare = request.form.get('fjern_vare')
+        conn = sqlite3.connect(database='database/ny_database.db')
+        query5 = f"DELETE FROM Lageroversigt WHERE id={fjern_vare}"
+        query6 = "SELECT ID, Produktnavn, Produktnummer, Antal, Mål, Producent, Produktkategori, Pris FROM Lageroversigt ORDER BY id ASC"
+        try:
+            cur = conn.cursor()
+            cur.execute(query5)
+            conn.commit()
+            cur.execute(query6)
+            data_rows = cur.fetchall()
+        except sqlite3.OperationalError as oe:
+            print(f"Transaction could not be processed: {oe}")
+        except sqlite3.IntegrityError as ie:
+            print(f"Integrity constraint violated: {ie}")
+        except sqlite3.ProgrammingError as pe:
+            print(f"You used the wrong SQL table: {pe}")
+        except sqlite3.Error as e:
+            print(f"Error calling SQL: {e}")
+        finally:
+            cur.close()
+            conn.close()
+
         
+
+        # Jacobs
+        # produktnavn = request.form.get('produktnavn')
+        # produktnummer = request.form.get('produktnummer')
+        # antal = request.form.get('antal')
+        # bredde = request.form.get('bredde')
+        # bredde_enhed = request.form.get('bredde_enhed')
+        # if bredde_enhed == "m":
+        #     bredde = "%.2f" % float(bredde)
+        # længde = request.form.get('længde')
+        # længde_enhed = request.form.get('længde_enhed')
+        # if længde_enhed == "m":
+        #     længde = "%.2f" % float(længde)
+        # mål = f"B: {bredde} {bredde_enhed} L: {længde} {længde_enhed}"
+        # producent = request.form.get('producent')
+        # produktkategori = request.form.get('produktkategori')
+        # pris = request.form.get('pris')
+        # produktinfo = [produktnavn, produktnummer, antal, mål, producent,produktkategori, pris]
+        # print(produktinfo)
+        
+        # match(produktnavn, produktnummer, antal, mål, producent,produktkategori, pris)
+        # Jacobs slut
 
     return render_template('lager.html', data_rows = data_rows)
 
--- test.py
+++ test.py
@@ -31,3 +31,192 @@ finally:
 for item in data_rows:
     for item in item:
         print(item)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# <h2>Tilføj vare:</h2>
+#     <form action="" method="POST">
+#         <label for="produktnavn">Produktnavn</label><br>
+#         <input type="text" id="produktnavn" name="produktnavn"><br>
+#         <br>
+
+#         <label for="produktnummer">Produktnummer</label><br>
+#         <input type="text" id="produktnummer" name="produktnummer"><br>
+#         <br>
+
+#         <label for="antal">Antal</label><br>
+#         <input type="text" id="antal" name="antal"><br>
+#         <br>
+
+#         <label for="længde">Længde</label><br>
+#         <input type="text" id="længde" name="længde"><br>
+#         <br>
+
+#         <label for="længde_enhed">Længdeenhed</label><br>
+#         <input type="text" id="længde_enhed" name="længde_enhed"><br>
+#         <br>
+
+#         <label for="bredde">Bredde</label><br>
+#         <input type="text" id="bredde" name="bredde"><br>
+#         <br>
+
+#         <label for="bredde_enhed">Bredde enhed</label><br>
+#         <input type="text" id="bredde_enhed" name="bredde_enhed"><br>
+#         <br>
+
+#         <label for="mål">Mål</label><br>
+#         <input type="text" id="mål" name="mål"><br>
+#         <br>
+
+#         <label for="producent">Producent</label><br>
+#         <input type="text" id="producent" name="producent"><br>
+#         <br>
+
+#         <label for="produktkategori">Produktkategori</label><br>
+#         <input type="text" id="produktkategori" name="produktkategori"><br>
+#         <br>
+
+#         <label for="pris">Pris</label><br>
+#         <input type="text" id="pris" name="pris"><br>
+#         <br>
+#         <input type="submit" value="Submit">
+#     </form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Nyeste
+
+# <table>
+#         <tr>
+#             <th></th>
+#             <th>Produktnavn</th>
+#             <th>Produktnummer</th>
+#             <th>Antal</th>
+#             <th>Mål</th>
+#             <th>Producent</th>
+#             <th>Produktkategori</th>
+#             <th>Pris</th>
+#         </tr>
+
+#         {% for item in data_rows %}
+#         <tr>
+#             <td><form action="" method="POST"><input type="submit" value="Fjern vare"></form></td>
+#             {% for i in item %}
+#             <td>{{ i }}</td>
+#             {% endfor %}
+#         </tr>
+#         {% endfor%}
+#     </table><br>
+#     <br>
+
+
+
+
+
+
+
+
+# <table>
+#         <tr>
+#             <th></th>
+#             <th>Produktnavn</th>
+#             <th>Produktnummer</th>
+#             <th>Antal</th>
+#             <th>Mål</th>
+#             <th>Producent</th>
+#             <th>Produktkategori</th>
+#             <th>Pris</th>
+#         </tr>
+
+#         {% for item in data_rows %}
+#         <tr>
+#             {% for i in range(data_rows|length) %}
+#             <td><form action="" method="POST"><input type="submit" value="bob"></form></td>
+#             {% for item in item %}
+#             <td>{{ item }}</td>
+#             {% endfor %}
+#         </tr>
+#         {% endfor%}
+#     </table><br>
+#     <br>
+
+
+
+
+
+# Det nyeste
+# <table>
+#         <tr>
+#             <th></th>
+#             <th>Produktnavn</th>
+#             <th>Produktnummer</th>
+#             <th>Antal</th>
+#             <th>Mål</th>
+#             <th>Producent</th>
+#             <th>Produktkategori</th>
+#             <th>Pris</th>
+#         </tr>
+
+#         {% for i in range(data_rows|length) %}
+#         <tr>
+#             <td><form action="" method="POST"><input type="submit" name="fjern_vare" value="{{ i }}"></form></td>
+#             {% for item in data_rows[i] %}
+#             <td>{{ item }}</td>
+#             {% endfor %}
+#         </tr>
+#         {% endfor%}
+#     </table>    
+
+
+
+
+
+
+
+
+# <table>
+#         <tr>
+#             <th></th>
+#             <th>ID</th>
+#             <th>Produktnavn</th>
+#             <th>Produktnummer</th>
+#             <th>Antal</th>
+#             <th>Mål</th>
+#             <th>Producent</th>
+#             <th>Produktkategori</th>
+#             <th>Pris</th>
+#         </tr>
+
+#         {% for i in range(data_rows|length) %}
+#         <tr>
+#             <td><form action="" method="POST"><input type="submit" name="fjern_vare" value=""></form></td>
+#             {% for item in data_rows[i] %}
+#             <td>{{ item }}</td>
+#             {% endfor %}
+#         </tr>
+#         {% endfor%}
+#     </table> 
